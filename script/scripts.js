663617e05f3c8156ab4dba561cde124f5303aff2
austinstafford <sta22005@byui.edu>
2022-09-18 23:34:46 -0600

    commit

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,37 @@
 querySelector 
-alert(document.lastModified);
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+let d = new Date();
+let day = days[d.getDay()];
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = `Last Modified on ${day}, ${month} ${d.getDate()}, ${year}. `
+
+document.getElementById("lastmodified").textContent = modifieddate;
+
+alert(document.lastmodified);
 // returns current date and time. 
+
