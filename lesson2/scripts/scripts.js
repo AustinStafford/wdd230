e21a798c6d21274462028745191705aa8f9a5518
austinstafford <sta22005@byui.edu>
2022-09-28 16:27:33 -0600

    commit

---
+++ lesson2/scripts/scripts.js
@@ -0,0 +1,6 @@
+let date = document.lastModified
+document.querySelector("#lastModified").textContent = date;
+
+let a = new Date();
+let b = a.getFullYear();
+document.getElementById("currentYear").textContent = b;
