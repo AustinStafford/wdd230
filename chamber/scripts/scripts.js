8b54d7bae1aaaa32c84a329677f1f601550934d2
austinstafford <sta22005@byui.edu>
2022-10-08 17:29:38 -0600

    commit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,7 +1,13 @@
+//let upToDate = new Date (lastModified)
+//let element = document.querySelector()
+
 let date = document.lastModified
 document.querySelector("#lastModified").textContent = date;
 
 let a = new Date();
 let b = a.getFullYear();
 document.getElementById("currentYear").textContent = b;
+
+let upToDate = new Date();
+let element = document.querySelector('#upToDate').textContent = upToDate;
 
