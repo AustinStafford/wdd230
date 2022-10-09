76746fddcc867d8ffc39e6841b09d2effa3c0089
austinstafford <sta22005@byui.edu>
2022-10-08 22:24:07 -0600

    commit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -11,3 +11,10 @@ document.getElementById("currentYear").textContent = b;
 let upToDate = new Date();
 let element = document.querySelector('#upToDate').textContent = upToDate;
 
+function togglerMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
