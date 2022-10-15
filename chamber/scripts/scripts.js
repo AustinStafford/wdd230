648efa59dd9d736d6693a48686610b49737c8682
austinstafford <sta22005@byui.edu>
2022-10-15 17:48:25 -0600

    commit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -14,3 +14,14 @@ let element = document.querySelector('#upToDate').textContent = upToDate;
 function toggleMenu() {
     document.getElementById("navButton").classList.toggle("hide");
 }
+
+//To change the display property in JavaScript, consider the use of .style.display = "block" 
+//in a selection structure where the condition looks at the day of the week through the Date() object and getDay() 
+//methodLinks to an external site.. Note that you most likely already have a date type variable that you can use given 
+//the current date and/or copyright year is published to the page using JavaScript.
+
+//Use JavaScript to display a banner on Mondays or Tuesdays only at the very top of the page that says 
+//"🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m." Make sure your design matches your schema for the site.
+
+
+const remindDate = a.getDay('Monday');
