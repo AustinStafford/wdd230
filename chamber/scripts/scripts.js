c8144f83619a09462de4d1c96fb02cc0e1ccc672
austinstafford <sta22005@byui.edu>
2022-11-04 20:31:22 -0600

    commit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -85,4 +85,25 @@ imagesToLoad.forEach((img) => {
 // page by the user's agent (browser). You may elect to 
 // display this information where you deem fit on the page.
 
-let visitDays = window.localStorage.setItem(vistis, numberOfVisits);
+// let visitDays = Number(window.localStorage.setItem("vistis", numberOfVisits));
+
+// totalVisits++;
+
+// localStorage.setItem("vistis", totalVisits);
+
+// initialize display elements
+// const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".user_visits");
+
+let totalVisits = Number(window.localStorage.getItem("visits-ls"));
+
+
+if (totalVisits !== 0) {
+	visitsDisplay.textContent = totalVisits;
+} 
+
+totalVisits++;
+
+localStorage.setItem("visits-ls", totalVisits);
+
+todayDisplay.textContent = Date.now();
