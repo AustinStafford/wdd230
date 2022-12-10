290b0c9642d3e45ee6eb0950d8a5a145a38d4829
austinstafford <sta22005@byui.edu>
2022-12-10 02:44:51 -0700

    commit

--- bountiful_foods/scripts/bountifulfoods.js
+++ bountiful_foods/scripts/bountifulfoods.js
@@ -1,3 +1,13 @@
+// ----------NAVIGATION---------- //
+
 function toggleMenu() {
     document.getElementById("navigation").classList.toggle("hide");
 }
+
+// ----------FOOTER---------- //
+let date = document.lastModified
+document.querySelector("#lastModified").textContent = date;
+
+let a = new Date();
+let b = a.getFullYear();
+document.getElementById("currentYear").textContent = b;
