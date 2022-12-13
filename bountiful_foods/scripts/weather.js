e155623c99159e030a54420bca4fa7bedd0103e8
austinstafford <sta22005@byui.edu>
2022-12-13 03:26:41 -0700

    commit

---
+++ bountiful_foods/scripts/weather.js
@@ -0,0 +1,40 @@
+const currentTemp = document.querySelector('#current-temp');
+const windSpeed = document.querySelector('#wind-speed');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=carlsbad&units=imperial&appid=62a5684b5a13d785bf0dcaaae0881b52'
+
+const windChill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16));
+if (currentTemp <= 50 && windSpeed > 3) {
+    document.getElementById('wind-chill').innerHTML = Math.round(windChill); 
+} else {
+    document.getElementById('wind-chill').innerHTML = "N/A";
+}
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); 
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
