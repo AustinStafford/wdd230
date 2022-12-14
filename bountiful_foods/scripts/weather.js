8ef33197e2ba72b303c8b02bc648ec4b8cae1f71
austinstafford <sta22005@byui.edu>
2022-12-14 03:24:11 -0700

    commit

--- bountiful_foods/scripts/weather.js
+++ bountiful_foods/scripts/weather.js
@@ -1,16 +1,17 @@
 const currentTemp = document.querySelector('#current-temp');
-const windSpeed = document.querySelector('#wind-speed');
+// const windSpeed = document.querySelector('#wind-speed');
 const humidity = document.querySelector('#humidity');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=carlsbad&units=imperial&appid=62a5684b5a13d785bf0dcaaae0881b52'
+const urlOther = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.1581&lon=-117.3506&exclude=daily&appid=62a5684b5a13d785bf0dcaaae0881b52'
 
-const windChill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16));
-if (currentTemp <= 50 && windSpeed > 3) {
-    document.getElementById('wind-chill').innerHTML = Math.round(windChill); 
-} else {
-    document.getElementById('wind-chill').innerHTML = "N/A";
-}
+// const windChill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16));
+// if (currentTemp <= 50 && windSpeed > 3) {
+//     document.getElementById('wind-chill').innerHTML = Math.round(windChill); 
+// } else {
+//     document.getElementById('wind-chill').innerHTML = "N/A";
+// }
 
 async function apiFetch() {
     try {
@@ -31,7 +32,7 @@ async function apiFetch() {
 
   function  displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-    windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
+    // windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
     humidity.innerHTML = `<strong>${weatherData.main.humidity.toFixed(0)}</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
