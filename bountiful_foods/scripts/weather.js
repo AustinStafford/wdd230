43588b8fb61fc5fb693c47ae4de291d0e7c73dc6
austinstafford <sta22005@byui.edu>
2022-12-14 00:30:15 -0700

    commit

--- bountiful_foods/scripts/weather.js
+++ bountiful_foods/scripts/weather.js
@@ -1,5 +1,6 @@
 const currentTemp = document.querySelector('#current-temp');
 const windSpeed = document.querySelector('#wind-speed');
+const humidity = document.querySelector('#humidity');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=carlsbad&units=imperial&appid=62a5684b5a13d785bf0dcaaae0881b52'
@@ -31,6 +32,7 @@ async function apiFetch() {
   function  displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
     windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
+    humidity.innerHTML = `<strong>${weatherData.main.humidity.toFixed(0)}</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
   
